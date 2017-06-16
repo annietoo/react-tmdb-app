36428dcc647f53ed265cd82ec5905fb437d84257
annietoo <vto2@uic.edu>
2017-06-16 18:13:15 -0500

    Fix Search Bar and add placeholder image for movie with no poster

--- src/components/search.js
+++ src/components/search.js
@@ -14,22 +14,20 @@ class Search extends Component {
 	}
 
 	componentDidMount() {
-		const id = {value: 297762};
+		const id = {value: 297762}; // Wonder Woman movie
 		this.onSelect(id);
 	}
 
 	fetchMovie(input) {
-		if (input !== '') {
-			return axios.get(`${ROOT_URL}search/movie?query=${input}&page=1&api_key=${API_KEY}`)
+		const query = (input === '') ? "  " : "";
+			return axios.get(`${ROOT_URL}search/movie?query=${input}${query}&page=1&api_key=${API_KEY}`)
 					.then((res) => {
-						console.log(res.data.results);
 						const results = res.data.results.map((result) => {
 								return {value: result.id, label: result.title}	
 							});
 						
 						return {options: results};
 					}).catch((error) => console.log(error));
-		}
 	}
 
 	onSelect(val) {
@@ -40,9 +38,10 @@ class Search extends Component {
 	}
 
 	render() {
+		const bgImg = (this.state.movie.backdrop_path) ? `url(http://image.tmdb.org/t/p/w1280${this.state.movie.backdrop_path})` : "";
 		return (
 
-			<section id="search" style={{backgroundImage: `url(http://image.tmdb.org/t/p/w1280${this.state.movie.backdrop_path})`}}>
+			<section id="search" style={{backgroundImage: bgImg}}>
 			    <div className="mask"></div>
 			    <div className="container">
 			    	<div className="row">
--- src/components/searchResult.js
+++ src/components/searchResult.js
@@ -3,13 +3,13 @@ import React from 'react';
 const SearchResult = (props) => {
 
 	const {genres, info} = props;
-
+	const poster = info.poster_path ? `http://image.tmdb.org/t/p/w500${info.poster_path}` : "http://via.placeholder.com/400x600";
 	return (
 		<div className="row">
 	        <div className="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2" >
 	            <div className="row search-result">
 	            	<div className="col-md-5 result-image">
-	              		<img className="img-fluid" src={`http://image.tmdb.org/t/p/w500${info.poster_path}`} alt="Poster"/>
+	              		<img className="img-fluid" src={poster} alt="Poster"/>
 	            	</div>
 	            	<div className="col-md-7 re">
 			            <div className="title"><h3>{info.title}</h3></div>
@@ -24,9 +24,9 @@ const SearchResult = (props) => {
 			            <p className="overview">{info.overview}</p>
 			            <div className="info">
 		                    <div><span>Original Release:</span> {info.release_date}</div>
-		                    <div><span>Box Office:</span> {info.runtime} mins</div>
-		                    <div><span>Runtime:</span> {info.vote_average} / 10</div>
-		                    <div><span>Vote Average:</span> ${parseInt(info.revenue, 0).toLocaleString()}</div>
+		                    <div><span>Runtime:</span> {info.runtime} mins</div>
+		                    <div><span>Vote Average:</span> {info.vote_average ? `${info.vote_average} / 10` : "-"} </div>
+		                    <div><span>Box Office:</span> {info.revenue ? `$${parseInt(info.revenue, 0).toLocaleString()}` : "-"}</div>
 	                	</div>
 	            	</div>
 	            </div>
